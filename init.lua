d83bb22d9d6b9c410ccae531ff824f7e0f1367cb
pithydon <daniel@danielimac.local>
2016-07-29 18:17:53 -0600

    fix the bug

--- init.lua
+++ init.lua
@@ -1,132 +1,111 @@
-local registered_combos = {}
-
-local register_combo = function(name, node_def)
-	local n_tiles = table.copy(node_def.tiles)
-
-	if not n_tiles[2] then
-		n_tiles[2] = n_tiles[1]
+comboblock = {index = {
+	["default:wood"] = "stairs:slab_wood",
+	["default:junglewood"] = "stairs:slab_junglewood",
+	["default:pine_wood"] = "stairs:slab_pine_wood",
+	["default:acacia_wood"] = "stairs:slab_acacia_wood",
+	["default:aspen_wood"] = "stairs:slab_aspen_wood",
+	["default:stone"] = "stairs:slab_stone",
+	["default:cobble"] = "stairs:slab_cobble",
+	["default:stonebrick"] = "stairs:slab_stonebrick",
+	["default:desert_stone"] = "stairs:slab_desert_stone",
+	["default:desert_cobble"] = "stairs:slab_desert_cobble",
+	["default:desert_stonebrick"] = "stairs:slab_desert_stonebrick",
+	["default:sandstone"] = "stairs:slab_sandstone",
+	["default:sandstonebrick"] = "stairs:slab_sandstonebrick",
+	["default:obsidian"] = "stairs:slab_obsidian",
+	["default:obsidianbrick"] = "stairs:slab_obsidianbrick",
+	["default:brick"] = "stairs:slab_brick",
+	["farming:straw"] = "stairs:slab_straw",
+	["default:steelblock"] = "stairs:slab_steelblock",
+	["default:copperblock"] = "stairs:slab_copperblock",
+	["default:bronzeblock"] = "stairs:slab_bronzeblock",
+	["default:goldblock"] = "stairs:slab_goldblock"
+}}
+local creative = minetest.setting_getbool("creative_mode")
+for k,v1 in pairs(comboblock.index) do
+	local v1_def = minetest.registered_nodes[v1]
+	local v1_groups = table.copy(v1_def.groups)
+	v1_groups.not_in_creative_inventory = 1
+	local v1_tiles = table.copy(v1_def.tiles)
+	if not v1_tiles[2] then
+		v1_tiles[2] = v1_tiles[1]
 	end
-	if not n_tiles[3] then
-		n_tiles[3] = n_tiles[2]
+	if not v1_tiles[3] then
+		v1_tiles[3] = v1_tiles[2]
 	end
-	if not n_tiles[4] then
-		n_tiles[4] = n_tiles[3]
+	if not v1_tiles[4] then
+		v1_tiles[4] = v1_tiles[3]
 	end
-	if not n_tiles[5] then
-		n_tiles[5] = n_tiles[4]
+	if not v1_tiles[5] then
+		v1_tiles[5] = v1_tiles[4]
 	end
-	if not n_tiles[6] then
-		n_tiles[6] = n_tiles[5]
+	if not v1_tiles[6] then
+		v1_tiles[6] = v1_tiles[5]
 	end
-
-	for _,v in ipairs(registered_combos) do
-		if name ~= v then
-			local v_def = minetest.registered_nodes[v]
-
-			local v_tiles = table.copy(v_def.tiles)
-
-			if not v_tiles[2] then
-				v_tiles[2] = v_tiles[1]
+	for _,v2 in pairs(comboblock.index) do
+		if v1 ~= v2 then
+			local v2_def = minetest.registered_nodes[v2]
+			local v2_tiles = table.copy(v2_def.tiles)
+			if not v2_tiles[2] then
+				v2_tiles[2] = v2_tiles[1]
 			end
-			if not v_tiles[3] then
-				v_tiles[3] = v_tiles[2]
+			if not v2_tiles[3] then
+				v2_tiles[3] = v2_tiles[2]
 			end
-			if not v_tiles[4] then
-				v_tiles[4] = v_tiles[3]
+			if not v2_tiles[4] then
+				v2_tiles[4] = v2_tiles[3]
 			end
-			if not v_tiles[5] then
-				v_tiles[5] = v_tiles[4]
+			if not v2_tiles[5] then
+				v2_tiles[5] = v2_tiles[4]
 			end
-			if not v_tiles[6] then
-				v_tiles[6] = v_tiles[5]
+			if not v2_tiles[6] then
+				v2_tiles[6] = v2_tiles[5]
 			end
-
-			minetest.register_node("comboblock:"..name:gsub(":", "_").."_onc_"..v:gsub(":", "_"), {
-				description = node_def.description.." on "..v_def.description,
-				tiles = {n_tiles[1], v_tiles[2],
-						v_tiles[3].."^("..n_tiles[3].."^comboblock_splitter.png^[makealpha:255,0,255)",
-						v_tiles[4].."^("..n_tiles[4].."^comboblock_splitter.png^[makealpha:255,0,255)",
-						v_tiles[5].."^("..n_tiles[5].."^comboblock_splitter.png^[makealpha:255,0,255)",
-						v_tiles[6].."^("..n_tiles[6].."^comboblock_splitter.png^[makealpha:255,0,255)"},
+			minetest.register_node("comboblock:"..v1:split(":")[2].."_onc_"..v2:split(":")[2], {
+				description = v1_def.description.." on "..v2_def.description,
+				tiles = {v1_tiles[1], v2_tiles[2],
+						v2_tiles[3].."^("..v1_tiles[3].."^comboblock_splitter.png^[makealpha:255,0,255)",
+						v2_tiles[4].."^("..v1_tiles[4].."^comboblock_splitter.png^[makealpha:255,0,255)",
+						v2_tiles[5].."^("..v1_tiles[5].."^comboblock_splitter.png^[makealpha:255,0,255)",
+						v2_tiles[6].."^("..v1_tiles[6].."^comboblock_splitter.png^[makealpha:255,0,255)"},
 				paramtype = "light",
 				paramtype2 = "facedir",
 				drawtype = "normal",
-				sounds = node_def.sounds,
-				groups = node_def.groups,
-				drop = name,
+				sounds = v1_def.sounds,
+				groups = v1_groups,
+				drop = v1,
 				after_destruct = function(pos, oldnode)
-					minetest.set_node(pos, {name = v, param2 = oldnode.param2})
+					minetest.set_node(pos, {name = v2, param2 = oldnode.param2})
 				end
 			})
-
-			-- bug is here
-			---[[
-			minetest.register_node("comboblock:"..v:gsub(":", "_").."_onc_"..name:gsub(":", "_"), {
-				description = v_def.description.." on "..node_def.description,
-				tiles = {v_tiles[1], n_tiles[2],
-						n_tiles[3].."^("..v_tiles[3].."^comboblock_splitter.png^[makealpha:255,0,255)",
-						n_tiles[4].."^("..v_tiles[4].."^comboblock_splitter.png^[makealpha:255,0,255)",
-						n_tiles[5].."^("..v_tiles[5].."^comboblock_splitter.png^[makealpha:255,0,255)",
-						n_tiles[6].."^("..v_tiles[6].."^comboblock_splitter.png^[makealpha:255,0,255)"},
-				paramtype = "light",
-				paramtype2 = "facedir",
-				drawtype = "normal",
-				sounds = v_def.sounds,
-				groups = v_def.groups,
-				drop = v,
-				after_destruct = function(pos, oldnode)
-					minetest.set_node(pos, {name = name, param2 = oldnode.param2})
-				end
-			})
-			--]]
 		end
 	end
-
-	table.insert(registered_combos, name)
+	minetest.override_item(v1, {
+		on_place = function(itemstack, placer, pointed_thing)
+			local pos = pointed_thing.under
+			if pointed_thing.type ~= "node" or minetest.is_protected(pos, placer:get_player_name()) then
+				return
+			end
+			local node = minetest.get_node(pos)
+			if node.name == v1 then
+				minetest.swap_node(pos, {name = k, param2 = 0})
+				if not creative then
+					itemstack:take_item()
+					return itemstack
+				end
+			else
+				for _,v in pairs(comboblock.index) do
+					if node.name == v then
+						minetest.swap_node(pos, {name = "comboblock:"..v1:split(":")[2].."_onc_"..v:split(":")[2], param2 = node.param2})
+						if not creative then
+							itemstack:take_item()
+							return itemstack
+						end
+						return
+					end
+				end
+				return minetest.item_place(itemstack, placer, pointed_thing, param2)
+			end
+		end,
+	})
 end
-
-for _,v in pairs(minetest.registered_nodes) do
-	local n = v.name
-	local n1 = n:split('_')[1]
-	if n1 == "stairs:slab" and v.paramtype2 == "facedir" then
-		register_combo(n, v)
-	end
-end
-
-function table.val_to_str ( v )
-  if "string" == type( v ) then
-    v = string.gsub( v, "\n", "\\n" )
-    if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
-      return "'" .. v .. "'"
-    end
-    return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
-  else
-    return "table" == type( v ) and table.tostring( v ) or
-      tostring( v )
-  end
-end
-
-function table.key_to_str ( k )
-  if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
-    return k
-  else
-    return "[" .. table.val_to_str( k ) .. "]"
-  end
-end
-
-function table.tostring( tbl )
-  local result, done = {}, {}
-  for k, v in ipairs( tbl ) do
-    table.insert( result, table.val_to_str( v ) )
-    done[ k ] = true
-  end
-  for k, v in pairs( tbl ) do
-    if not done[ k ] then
-      table.insert( result,
-        table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
-    end
-  end
-  return "{" .. table.concat( result, "," ) .. "}"
-end
-
-minetest.log("warning", "[comboblock] "..table.tostring(registered_combos))
